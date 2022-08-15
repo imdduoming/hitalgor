94e39b976644211aa8f8e30b967292276a3777fb
dduoming <60255575+imdduoming@users.noreply.github.com>
2022-08-15 12:18:22 +0900

    백준 알고리즘 카카오 기출 보석쇼핑 L3

--- 보석쇼핑.py
+++ 보석쇼핑.py
@@ -1,34 +1,40 @@
-# def solution(gems):
-#     answer = []
-#     org_dict={}
-#     for i in gems:
-#         if i not in org_dict:
-#             org_dict[i] = 1
-#     gems_type=len(set(gems))
-#     dict={}
-#     left=0
-#     right=0
-#     while right<len(gems):
-#         if gems[right] not in dict:
-#             dict[gems[right]]=1
-#             if len(dict)==gems_type:
-#                 break
-#             else:
-#                 right+=1
-#         else:
-#             dict[gems[right]]+=1
-#             right+=1
-#
-#     while left<len(gems):
-#         if len(dict)==gems_type:
-#             dict[gems[left]]-=1
-#             if dict[gems[left]]==0:
-#                 break
-#             else:
-#                 left+=1
-#
-#     answer.append(left+1)
-#     answer.append(right+1)
-#     return answer
-#
-# print(solution(["A","B","B","B","B","B","B","C","B","A"]))
+def solution(gems):
+    import sys
+    answer = [1,len(gems)]
+    org_dict={}
+    for i in gems:
+        if i not in org_dict:
+            org_dict[i] = 1
+    gems_type=len(set(gems))
+    dict={}
+    left=0
+    right=0
+    ans=sys.maxsize
+    dict[gems[0]]=1
+    while right<len(gems):
+        if len(dict)<gems_type:
+            right+=1
+            # print('right',right)
+            if right<len(gems):
+                if (gems[right] not in dict) :
+                    dict[gems[right]]=1
+
+                else:
+                    dict[gems[right]]+=1
+
+
+        else:
+            if answer[1]-answer[0]>right-left:
+                answer=[left+1,right+1]
+                # print(answer)
+            if dict[gems[left]]==1:
+                del dict[gems[left]]
+            else:
+                dict[gems[left]]-=1
+            left+=1
+            # print('left',left)
+        # print(dict)
+
+    return answer
+
+print(solution(["A","B","B","B","B","B","B","C","B","A"]))
